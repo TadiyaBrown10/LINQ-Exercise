416a01ccc43a5a15379d4088c52409e92300c056
Tadiya Brown <tadiyabrown2000@gmail.com>
2023-04-30 23:28:35 -0400

    LINQ one complete

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,31 @@
 ﻿namespace LINQ
 {
+    using System.Collections.Generic;
+    using System.Linq;
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> nailPolishcolor = new List<string>();
+
+            nailPolishcolor.Add("Black");
+            nailPolishcolor.Add("Gray");
+            nailPolishcolor.Add("Red");
+            nailPolishcolor.Add("Blue");
+            nailPolishcolor.Add("Yellow");
+
+            var inOrder = nailPolishcolor.OrderBy(polish => polish.Length);
+
+            foreach(var name in inOrder)
+            {
+                Console.WriteLine(name);
+
+            }
         }
+
+        
+
+       
+
     }
 }
